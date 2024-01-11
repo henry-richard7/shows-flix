adaa721ab9e3b65a982444fe7ba301a41722b33e
Henry Richard <henry.richard.2k@gmail.com>
2024-01-11 09:57:14 +0000

    Added InkWell to cards

--- lib/Components/home_page_widgets.dart
+++ lib/Components/home_page_widgets.dart
@@ -12,16 +12,19 @@ class HomePageWidgets {
           itemCount: pageData.length,
           itemBuilder: (builder, index) {
             return Card(
-              child: Column(
-                children: [
-                  Flexible(
-                      child: Image.network(
-                    pageData[index]['image'],
-                    scale: 0.1,
-                  )),
-                  Text(pageData[index]['title']),
-                  Text(pageData[index]['release_date'])
-                ],
+              child: InkWell(
+                child: Column(
+                  children: [
+                    Flexible(
+                        child: Image.network(
+                      pageData[index]['image'],
+                      scale: 0.1,
+                    )),
+                    Text(pageData[index]['title']),
+                    Text(pageData[index]['release_date'])
+                  ],
+                ),
+                onTap: () => print(pageData[index]['link']),
               ),
             );
           }),
